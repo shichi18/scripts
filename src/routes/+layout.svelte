eef328d8f494f2fd0ba46bd60213181a06d4fab1
shichi <n.shichi18@gmail.com>
2024-07-08 06:23:04 +0900

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+</script>
+
+<main class="hero">
+	<div class="page">
+		<slot />
+	</div>
+</main>
+
+<style>
+    .hero {
+        position: relative;
+        height: 100vh;
+        background: url('/images/internet.jpg') no-repeat center center;
+        background-size: cover;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        text-align: center;
+        transition: opacity 1s;
+        opacity: 1;
+        width: 100%;
+        overflow: hidden;
+    }
+
+    .hero::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(30, 33, 72, 0.3); /* オーバーレイの色と透明度 */
+    }
+
+    .page {
+        position: relative;
+        transition: opacity 1.5s;
+
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<div>
+		<h1>Script Collection</h1>
+		<p>便利なスクリプト集</p>
+	</div>
+	<div>
+		<p><a href="/csv-to-json">CSV to JSON </a></p>
+		<p><a href="/json-to-csv">Json to CSV </a></p>
+	</div>
+</div>
+
+
+<style>
+     a{
+        font-weight:bold;
+        display: inline-block;
+        transition:.3s;
+        transform: scale(1);
+				color: #dddddd;
+    }
+     a:hover{
+        font-weight:bold;
+        transform: scale(1.05);
+        color:#f89174;/*文字色*/
+    }
+</style>
